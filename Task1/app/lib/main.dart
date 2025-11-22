968557b61ce1b1f1b078375dd7b5064e6404eb43
Nihil007 <nihilrajan2004@gmail.com>
2025-11-22 11:56:51 +0530

    minor change

--- Task1/app/lib/main.dart
+++ Task1/app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Auth Demo',
+      title: 'Login',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.indigo),
       home: const LoginPage(),
